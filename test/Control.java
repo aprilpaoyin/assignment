c10026ca3fa0c954831d0d94dfa6d08cce077d4d
aprilpaoyin <aprilpaoyin@gmail.com>
2016-03-31 17:52:03 +0100

    Added files via upload

---
+++ test/Control.java
@@ -0,0 +1,13 @@
+package com.ass1.test;
+
+public class Control 
+{
+
+	public static void main(String[] args) 
+	{
+		Screen window1 = new Screen("RoboReader v1.0");
+		window1.setSize(400, 400);
+		window1.setLocation(430, 170);
+	}
+
+}
---
+++ test/Screen.java
@@ -0,0 +1,149 @@
+/*
+ * CODE: 		SCREEN (GUI)
+ * DESCRIPTION:	Allows user to enter address of txt file, then displays a pop-up that shows words occurred in 
+ * 				txt file and the frequency of it. 
+ * AUTHOR: 		APRIL TAN
+ * DATE: 		28 MAR 2016
+ * 
+ */
+
+
+package com.ass1.test;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.*;
+
+
+public class Screen extends JFrame implements ActionListener, MouseListener
+{
+	//private JTextField field2;
+	private JTextField field;
+	private JButton button1;
+	//private JButton button2;
+	private String str1;
+	private String str2;
+
+//constructor
+	public Screen(String title)
+	{
+		super(title);
+		
+		setLayout(new FlowLayout());	
+		
+		field = new JTextField("Enter address of text file.");
+	    field.setPreferredSize( new Dimension( 200, 30 ) );
+	    field.addMouseListener(this);
+		add(field);
+		
+		/*
+		field2 = new JTextField();
+	    field2.setPreferredSize( new Dimension( 100, 24 ) );
+		add(field2);
+		*/
+		
+		button1 = new JButton("Read this file.");
+		add(button1);
+		
+		//button2= new JButton("Not Error Checking");
+		//add(button2);
+		
+		
+		button1.addActionListener(this);
+		//mouseClicked(this)
+		addMouseListener(new MyMouseListener(this));
+
+		//button2.addActionListener(this);
+
+		
+		setVisible(true);
+		
+
+	}
+
+
+
+
+	public void actionPerformed(ActionEvent arg0) {
+
+		if(arg0.getSource()==button1)
+		{
+			str1=field.getText();
+			//str2=field2.getText();
+			
+			if(str2.length()==1)
+			{
+				
+				try
+				{
+					if(str1.charAt(0)==str2.charAt(0))
+					{
+						JOptionPane.showMessageDialog(this,"They Match");	
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(this, "They Dont Match");
+		
+					}
+				}
+				catch (IndexOutOfBoundsException e)
+				{
+					    System.err.println("IndexOutOfBoundsException: " + e.getMessage());
+				}
+			}
+			
+			else{
+				JOptionPane.showMessageDialog(this, "Please only enter 1 character");
+			}
+		}
+		
+		/*if(arg0.getSource()==button2)
+		{
+			JOptionPane.showMessageDialog(this, "THIS AINT ERROR CHECKING DWAG");
+		}*/
+
+		
+	}
+	
+	public void mousePressed(MouseEvent e){
+	
+	}
+
+
+	public void mouseClicked(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		field.setText("");
+	}
+
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
