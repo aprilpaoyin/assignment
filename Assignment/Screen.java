882273ac1cf2956df523141992a6573b38bfdb17
aprilpaoyin <aprilpaoyin@gmail.com>
2016-04-11 09:27:42 +0100

    Added files via upload

--- Assignment/Screen.java
+++ Assignment/Screen.java
@@ -11,19 +11,27 @@ package com.assignment.Assignment;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.File;
-
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class Screen extends JFrame implements ActionListener//, MouseListener
+public class Screen extends JFrame implements ActionListener, ReadFile
 {
-		private JTextField field; //shows address of file
-		private JButton button1; //to confirm, read a file
-		private JButton button2; //to browse for a file
-		private String str1; //address of file in a str
-		private JLabel label1; //label for the txt field
+		protected JTextField field; //shows address of file
+		protected JButton button1; //to browse for a file
+		protected JButton button2; //to confirm, read a file
+		private JLabel label; //label for the txt field
+		static File file; //declaration for file
+		static ArrayList<String> words = new ArrayList<String>(); //array to store words
+	    static ArrayList<Integer> counts = new ArrayList<Integer>(); //array to store words count
+		static ArrayList<String> combined = new ArrayList<String>(); //array to store combined words and count to display
+	    
+		/*
+		 * Code for FileChooser referenced from: https://docs.oracle.com/javase/tutorial/uiswing/components/filechooser.html
+		 */
 		final JFileChooser fChooser; //let user choose a file
 		
 		//constructor
@@ -31,10 +39,9 @@ public class Screen extends JFrame implements ActionListener//, MouseListener
 		{
 			//put a title for screen
 			super(title);
-			
 			setLayout(new FlowLayout());
 			
-			label1 = new JLabel("Choose a text file");
+			label = new JLabel("Choose a text file");
 			field = new JTextField("File name");
 		    field.setPreferredSize( new Dimension( 200, 30 ));
 			fChooser = new JFileChooser();
@@ -42,13 +49,13 @@ public class Screen extends JFrame implements ActionListener//, MouseListener
 			//set font style of text field
 			field.setFont(new Font("Arial", Font.ITALIC, 12));
 			
-			button1 = new JButton("Read this file");
-			button2 = new JButton("Browse");
+			button1 = new JButton("Browse");
+			button2 = new JButton("Read this file");
 			
-			add(label1);
+			add(label);
 		    add(field);
-		    add(button2);
-			add(button1);
+		    add(button1);
+			add(button2);
 			
 			button1.addActionListener(this);
 			button2.addActionListener(this);
@@ -56,49 +63,141 @@ public class Screen extends JFrame implements ActionListener//, MouseListener
 			setVisible(true);
 		}//end 1st constructor
 
+		/*
+		 * -Checks source of the action event (button pressed).
+		 * -If button1 (browse) pressed, a dialog opens that allows users to select 1 .TXT FILE,
+		 * then displays a message that tells user file is being opened (no error).
+		 * -If button2 (read this file) pressed, a dialog opens that reads the file
+		 * and stores the words in an array.
+		 * 
+		 */
 		public void actionPerformed(ActionEvent arg0) 
 		{
-
-			if(arg0.getSource()==button1)
+			if(arg0.getSource() == button1)
 			{
-				setStr1(field.getText());
-			}
-			else if (arg0.getSource() == button2)
-			{
-				int returnVal = fChooser.showOpenDialog(this);
+				/*
+				 *File Filter code referenced from: http://stackoverflow.com/questions/15771949/how-do-i-make-jfilechooser-only-accept-txt
+				 * 
+				 * This code sets a filter so that users can only select .txt files to read.
+				 */
+				FileNameExtensionFilter txtFilter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
+				fChooser.setFileFilter(txtFilter);
 				
+				//File Chooser code referenced from : https://docs.oracle.com/javase/tutorial/uiswing/components/filechooser.html
+				int returnVal = fChooser.showOpenDialog(this);
+			
 				if(returnVal == JFileChooser.APPROVE_OPTION)
 				{
-					File file = fChooser.getSelectedFile();
-					JOptionPane.showMessageDialog(this, "Opening: " + file.getName());
+					//takes selected file and assigns it to file
+					file = fChooser.getSelectedFile();
+					JOptionPane.showMessageDialog(this, "Opening: " + file.getName(), "Opening File", JOptionPane.INFORMATION_MESSAGE);
+					
+					//put file name in test field
 					field.setText(file.getName());
 					field.setFont(new Font("Arial", Font.PLAIN, 12));
-				}
+				}//end inner if
 				else
 				{
-					JOptionPane.showMessageDialog(this, "CANCELLED");
-				}
-			}
-			
+					JOptionPane.showMessageDialog(this, "CANCELLED", "Process Cancelled", JOptionPane.INFORMATION_MESSAGE);
+				}//end else
+			}//end outer if
+			else if (arg0.getSource() == button2)
+			{
+				try
+				{
+					//calls read method to read a file and store words in array
+					read(file);
+				}//end try
+				catch (Exception e)
+				{
+					JOptionPane.showMessageDialog(this, "Error: " + e);
+				}//end catch
+			}//end else...if
 		}//end actionlistener
+		
+		/*
+		 * Reads file word by word
+		 */
+		public static void read(File file) throws FileNotFoundException
+		{
+			Scanner scan = new Scanner(file); 
+		    String holder; //holder for word
+		    String singleWord; //used for comparison
+		    int searchIx; //search index
+		    boolean newWord; //whether or not current word is a new word
+			
+			//put first word into array 
+			holder = scan.next();
+			//replace anything in string that aren't letters or numbers
+			holder = holder.replaceAll("[^a-zA-Z0-9]", "");
+			holder = holder.toLowerCase();
+			words.add(holder);
+			counts.add(1);
+			
+			while(scan.hasNext())
+			{	   
+				holder = scan.next();
+				holder = holder.toLowerCase();
+				singleWord = holder.replaceAll("[^a-zA-Z0-9]", "");
+				singleWord = singleWord.toLowerCase();
+				searchIx = 0;
+				newWord = true;
+				
+				//cycle through words array
+				for(String string : words)
+				{
+					//if current word is found in the words array
+					if(string.equals(singleWord))
+					{
+						/*increment count, set element in that index to the incremented number, 
+						 *then say that it is not a new word 
+						 */
+						int num = counts.get(searchIx);
+						num++;
+						counts.set(searchIx, num);
+						newWord = false;
+					}//end if
+					
+					searchIx++;
+				}//end for
+				
+				//if it is a new word (not in words array)
+				if(newWord)
+				{
+					words.add(singleWord);
+					counts.add(1);
+				}//end if
+				
+			}//end while
+			  
+			/*combine the contents of words and counts
+			 * 
+			 * code referenced from: http://stackoverflow.com/questions/16984221/display-arraylist-contents-in-a-joptionpane-showmessagedialog
+			 */
+	        StringBuilder combined = new StringBuilder("<html>");
+	        int elsPerLine = 0;
+			for(int i = 0; i < words.size(); i++)
+			{
+				combined.append(words.get(i) + ": " + counts.get(i) + "\t\t");
+				elsPerLine++;
+				if(elsPerLine > 2) 
+				{
+					combined.append("<br>");
+					elsPerLine = 0;
+				}//end if
+			}//end for
+			combined.append("</html>");
+			JOptionPane.showMessageDialog(null, combined.toString(), "Results", JOptionPane.INFORMATION_MESSAGE);
+			scan.close();
+		}//end read
 		
 		public JLabel getLabel1() 
 		{
-			return label1;
+			return label;
 		}
 
-		public void setLabel1(JLabel label1) 
+		public void setLabel1(JLabel label) 
 		{
-			this.label1 = label1;
-		}
-
-		public String getStr1() 
-		{
-			return str1;
-		}
-
-		public void setStr1(String str1) 
-		{
-			this.str1 = str1;
+			this.label = label;
 		}
 }
